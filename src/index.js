2367ec09b837b38fb74549b221f6e28efee99310
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-28 14:51:03 +0200

    create-app'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1> project templates, test </h1>, document.getElementById('root'));
+//ReactDOM.render(<h1> project templates, test </h1>, document.getElementById('root'));
+
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
